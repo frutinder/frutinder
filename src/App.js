f44fc28077947548f047b375a70efc7c96573f45
Tebs <qatrera@gmail.com>
2019-04-28 11:23:33 +0200

    Remove warnings

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import React from "react";
 import "./App.css";
 
 import Game from "./Game";
-import Score from "./components/Score";
 
-// const App = () => <Score finalScore={129382} />;
 const App = () => <Game />;
 
 export default App;
--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 
 const PrimaryButton = styled.button`
